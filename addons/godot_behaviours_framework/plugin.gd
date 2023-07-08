08b0ffaea6c878975bfdf61f938dc7b73b71c77f
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2023-07-07 23:43:50 -0300

    Create plugin script & config file

---
+++ addons/godot_behaviours_framework/plugin.gd
@@ -0,0 +1,20 @@
+@tool
+extends EditorPlugin
+
+
+#const BehaviourManagerAutoload: Dictionary = {
+#	"name": "BehaviourManager",
+#	"path": "res://addons/godot_behaviours_framework/core/behaviour_manager.gd",
+#}
+
+
+func _enter_tree():
+	pass
+#	add_autoload_singleton(
+#		BehaviourManagerAutoload["name"],
+#		BehaviourManagerAutoload["path"])
+
+
+func _exit_tree():
+	pass
+#	remove_autoload_singleton(BehaviourManagerAutoload["name"])
