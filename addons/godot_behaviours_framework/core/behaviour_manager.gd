c1a9217aef239f586aafdfda684df5ed24c48172
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2023-09-19 14:25:19 -0300

    Implement global Behaviour Manager

---
+++ addons/godot_behaviours_framework/core/behaviour_manager.gd
@@ -0,0 +1,13 @@
+extends Node
+
+
+const BehaviourData: Dictionary = {
+	Behaviour.Type.BEHAVIOUR: {
+		"script": "res://addons/godot_behaviours_framework/core/behaviour.gd",
+		"id": "Behaviour",
+	},
+}
+
+
+func _ready() -> void:
+	pass
